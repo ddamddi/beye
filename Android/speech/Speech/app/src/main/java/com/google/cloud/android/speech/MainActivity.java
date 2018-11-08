171a6cf9cc154284e5b24565a577e8cdea8566d2
ParkJinHyuk <cokka123@naver.com>
2018-11-08 21:44:27 +0900

    Add Bluetooth(toDo)
    add Bluetooth permission..

--- Android/speech/Speech/app/src/main/java/com/google/cloud/android/speech/MainActivity.java
+++ Android/speech/Speech/app/src/main/java/com/google/cloud/android/speech/MainActivity.java
@@ -18,6 +18,7 @@ package com.google.cloud.android.speech;
 
 import android.Manifest;
 import android.app.FragmentManager;
+import android.bluetooth.BluetoothAdapter;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
@@ -94,6 +95,7 @@ public class MainActivity extends AppCompatActivity implements MessageDialogFrag
 
     private final int PERMISSIONS_ACCESS_FINE_LOCATION = 1000;
     private final int PERMISSIONS_ACCESS_COARSE_LOCATION = 1001;
+    private final int REQUEST_ENABLE_BT = 1;
 
     private SpeechService mSpeechService;
 
@@ -162,6 +164,26 @@ public class MainActivity extends AppCompatActivity implements MessageDialogFrag
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if(mBluetoothAdapter == null) {
+            //장치가 블루투스를 지원하지 않는 경우.
+            finish();   // 어플리케이션 종료
+        }
+
+        else {
+            // 장치가 블루투스를 지원하는 경우.
+            if(!mBluetoothAdapter.isEnabled()) {
+                // 블루투스를 지원하지만 비활성 상태인 경우
+                // 블루투스를 활성 상태로 바꾸기 위해 사용자 동의 요첨
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            }
+
+            else {
+                // 블루투스를 지원하며 활성 상태인 경우
+                // 페어링된 기기 목록을 보여주고 연결할 장치를 선택.
+            }
+        }
 
 
         btnShowLocation = (Button) findViewById(R.id.btn_start);
@@ -223,9 +245,6 @@ public class MainActivity extends AppCompatActivity implements MessageDialogFrag
                     startLatitude = gps.getLatitude();
                     startLongitude = gps.getLongitude();
                     findRoute(startLatitude, startLongitude, desLatitude, desLongitude);
-//                    tmapApi.setLatitude(latitude);
-//                    tmapApi.setLongitude(longitude);
-//                    tmapApi.findRoute();
 
                     txtLatitude.setText(String.valueOf(startLatitude));
                     txtLongitude.setText(String.valueOf(startLongitude));
